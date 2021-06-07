a1d6e264e928285c71812d1fa4c01dad602c6ce9
Liam Hsu <smile176223@gmail.com>
2021-06-07 11:08:39 +0800

    Initial Commit

---
+++ VideoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoTest
+//
+//  Created by Hsu Hao Chun on 2021/6/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
